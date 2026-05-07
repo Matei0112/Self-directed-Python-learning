c8cb8e700c313eef81ff3dca26fd10fcecc5a48a
Matei0112 <mateimitrea22@gmail.com>
2026-05-07 06:44:05 +0300

    Add files via upload

---
+++ first script.py
@@ -0,0 +1,34 @@
+first_name="Matei"
+food=("Burger")
+Email=("realemail@gmail.com")
+print(f"Hello {first_name}")
+
+print(f"Do you like {food}?")
+
+print(f"I have figured out your email it is {Email}, you have 14 business days to pay the ransom in {food}]")
+
+#Integers
+
+age = 22
+quantity = 3
+print(f"You are {age} years old")
+print(f"you are buying {quantity} {food}")
+
+num_of_students= 30
+
+print(f"your class has {num_of_students} students")
+#float
+distance= 5.5
+price = 10.99
+GPA= 3.2
+print(f"the price is £ {price}")
+print(f"your GPA is {GPA}")
+print(f"you ran {distance} miles")
+
+#booleans
+
+is_student = True
+
+print(f"are you a student {is_student}")
+if is_student:print("you are a student")
+else:print("you are not a student")
