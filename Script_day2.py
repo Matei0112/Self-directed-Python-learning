2f72ccf26f4b0a44cff6c19bae846585353c07b0
Matei0112 <mateimitrea22@gmail.com>
2026-05-08 07:09:47 +0300

    Add files via upload

---
+++ Script_day2.py
@@ -0,0 +1,25 @@
+#typecasting Conversaion of variable from one data type to another
+
+name="Mate"
+print(type(name))
+
+
+age=22
+gpa=3.9999
+
+is_student = True
+
+type(name)
+
+gpa = int(gpa)
+
+print(gpa)
+
+age = str(age)
+
+age += "1"
+
+print(age)
+
+name = bool(name)
+print(name)
