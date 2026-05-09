a3179670f19f9c2928023ff9dff1ac2deb06ae4b
Matei0112 <mateimitrea22@gmail.com>
2026-05-09 07:02:52 +0300

    Add files via upload

---
+++ script_day3.py
@@ -0,0 +1,31 @@
+#Input ()= A function that prompts the user to enter data, returns the entered data as a string
+
+#=input("what is your name?: ")
+#age=int(input("How old are you?: "))
+
+#age= age+1
+#(f"hello {name}")
+
+#print(f"Are you {age} years old Today?")
+#print("HAPPY BIRTHDAY")
+
+#Exercise 1, Calculate the area of a rectangle
+
+#length=int(input("What is the length?: "))
+#width=int(input("What is the width?: "))
+
+#Area=length*width
+
+#print(f"The area is {Area}cm")
+
+#exercise 2 Shopping cart
+
+item=input("What are you trying to purchase?: ")
+
+price=float(input("What is the price?: "))
+
+quantity=int(input("How many would you like?: "))
+
+total=price*quantity
+
+print(f"That will be {total} pounds please")
